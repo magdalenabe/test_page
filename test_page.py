01454b38e63e2a5c99f072102a254105560e0e8f
Magdalena B <magdalena.behra@gmail.com>
2021-10-04 23:41:30 +0200

    aktualizacja projektu i bibliotek

--- test_page.py
+++ test_page.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+import time
 import pytest
 
 @pytest.fixture()
-def test_browser():
+def open_close():
     global driver
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
@@ -10,9 +11,10 @@ def test_browser():
     yield
     driver.close()
     driver.quit()
-    print("Test zakończony")
+    print("The test is completed")
 
-def test_search(test_browser):
+# spawdzenie funkcjonalności wyszukiwania wielu przedmiotów u jednegp sprzedawcy
+def test_search(open_close):
     driver.get("https://allegro.pl/")
     accept_cookies = driver.find_element_by_xpath("/html/body/div[2]/div[8]/div/div/div/div/div[2]/div[2]/button[1]")
     accept_cookies.click()
@@ -20,9 +22,26 @@ def test_search(test_browser):
     driver.find_element_by_id("input0").send_keys("mapa")
     driver.find_element_by_id("input1").send_keys("przewodnik turystyczny")
     driver.find_element_by_css_selector("button.m7er_k4").click()
+    time.sleep(3)
 
+# sprawdzenie funkcji "nie pamiętma hasła" podczas próby logowania
+def test_trylogin(open_close):
+    driver.get("https://allegro.pl/")
+    accept_cookies = driver.find_element_by_xpath("/html/body/div[2]/div[8]/div/div/div/div/div[2]/div[2]/button[1]")
+    accept_cookies.click()
+    driver.find_element_by_css_selector(".opbox-btn").click()
+    driver.find_element_by_css_selector("a.lc0vi:nth-child(1)").click()
+    driver.find_element_by_id("entry-input").send_keys("testing@com.pl")
+    time.sleep(3)
+  
+# wejście na podstronę "https://about.allegro.eu/" 
+def test_about(open_close):
+    driver.get("https://allegro.pl/")
+    accept_cookies = driver.find_element_by_xpath("/html/body/div[2]/div[8]/div/div/div/div/div[2]/div[2]/button[1]")
+    accept_cookies.click()
+   
+    action = webdriver.ActionChains(driver)
+    driver.execute_script("window.scrollTo(0, document. body. scrollHeight)") 
+    driver.find_element_by_link_text("O nas").click()
+    time.sleep(3)
 
-# def test_done():
-#     driver.close()
-#     driver.quit()
-#     print("Test zakończony")
